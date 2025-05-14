a607fc31526ea992f213b976733d38dcaacb4cf1
Tom Li <47617852+ttomli@users.noreply.github.com>
2025-05-13 20:19:17 -0500

    Emphasize hello world and added array

--- hello.c
+++ hello.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World!\n");
+    printf("Hello World!!\n");
+
+    
+    // ─────── Example 3: Out‐of‐bounds array access ───────
+    int arr[3] = {1,2,3};
+    arr[5] = 99;     // → SIGSEGV (often a crash)
 
     return 0;
 }
